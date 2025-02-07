8a14b49ff552e3e4fc029af7eaf460bb0679a71a
oceeboy <osieta33@gmail.com>
2025-02-07 00:06:52 -0500

    refactor: production

--- constants/bankdetails.ts
+++ constants/bankdetails.ts
@@ -1,8 +1,8 @@
 const bankDetail = {
-  bankName: "De Finanzbank",
-  bankAddress: "123 Main St, New York, NY 10001",
-  bankPhone: "123-456-7890",
-  bankEmail: "support@definanzrep.com",
+  bankName: "Berliner bank",
+  bankAddress: "8825 PINE BAY CT, ORLANDO, United States, 32825-3712",
+  bankPhone: "813-561-1342",
+  bankEmail: "support@berlinerrep.com",
   bankRoutingNumber: "123456789",
   bankAccountNumber: "1234567890",
 };
